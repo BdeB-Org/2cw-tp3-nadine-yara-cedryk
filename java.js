21536a98d188c89c938d734b5ecf9d1e2a1b589b
NADINE <2344893@bdeb.qc.ca>
2024-05-23 14:54:37 -0400

    condition dans JavaScript pour afficher selon les criteres demandés

--- java.js
+++ java.js
@@ -3,8 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchButton = document.getElementById('search-button');
     const searchNom = document.getElementById('search-nom');
     const searchDateNaissance = document.getElementById('search-date-naissance');
-    
+
     function fetchAuteurs() {
         let url = 'http://127.0.0.1:8080/ords/bibliotheque/auteur/';
         const nom = searchNom.value;
         const dateNaissance = searchDateNaissance.value;
+
+        if (nom) {
+            url += `?q={"nom":{"$like":"%${nom}%"}}`;
+        }
+        if (dateNaissance) {
+            url += nom ? '&' : '?';
+            url += `q={"date_naissance":"${dateNaissance}"}`;
+        }

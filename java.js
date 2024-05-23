1ca56041282d93ab686c9dc046b9df74bc4921b0
NADINE <2344893@bdeb.qc.ca>
2024-05-23 14:53:12 -0400

    javaScript modification

---
+++ java.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const auteursList = document.getElementById('auteurs-list');
+    const searchButton = document.getElementById('search-button');
+    const searchNom = document.getElementById('search-nom');
+    const searchDateNaissance = document.getElementById('search-date-naissance');

234f3ba63fda2428007aef17b517d0bfa4e4d8fa
NADINE <2344893@bdeb.qc.ca>
2024-05-29 23:39:08 -0400

    Update genres.js

--- genres.js
+++ genres.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const genresList = document.getElementById('genres-list');
     const searchButton = document.getElementById('search-button');
     const searchInput = document.getElementById('search-nom');
+    function fetchGenres(query = '') {
+        let url = 'http://127.0.0.1:8080/ords/bibliotheque/genres/';
+        if (query) {
+            url += `?q={"nom":{"$like":"%${query}%"}}`;
+        }

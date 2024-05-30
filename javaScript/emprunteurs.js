37ea364a00842b29f19fa9f3e07639292f82b2b6
NADINE <2344893@bdeb.qc.ca>
2024-05-30 10:25:32 -0400

    Update emprunteurs.js

--- javaScript/emprunteurs.js
+++ javaScript/emprunteurs.js
@@ -12,8 +12,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">${emprunteur.nom}</h5>
-                        <p class="card-text">Adresse: ${emprunteur.adresse}</p>
-                        <p class="card-text">Téléphone: ${emprunteur.telephone}</p>
+                        
+                        <p class="card-text">Adresse: ${emprunteur.email}</p>
+                        <p class="card-text">Téléphone: ${emprunteur.numero_telephone}</p>
                     </div>
                 </div>
             `;

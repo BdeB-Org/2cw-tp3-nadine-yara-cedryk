9a0c3d6398a12e8722e779f96b14ecce4ede2c1e
NADINE <2344893@bdeb.qc.ca>
2024-05-29 23:55:33 -0400

    Update prets.js

--- javaScript/prets.js
+++ javaScript/prets.js
@@ -9,3 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
                     const col = document.createElement('div');
                     col.className = 'col-md-4';
                     col.innerHTML = `
+                    <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Prêt ID: ${prêt.id_prêt}</h5>
+                        <p class="card-text">Date de prêt: ${prêt.date_prêt}</p>
+                        <p class="card-text">Date de retour: ${prêt.date_retour}</p>
+                        <p class="card-text">Emprunteur ID: ${prêt.emprunteurs_id_emprunteur}</p>
+                        <p class="card-text">Livre ID: ${prêt.livres_id_livre}</p>
+                    </div>
+                </div>

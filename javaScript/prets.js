2c10b1f001105783e669644d9aa7d89a9a70136d
NADINE <2344893@bdeb.qc.ca>
2024-05-29 23:55:13 -0400

    Update prets.js

--- javaScript/prets.js
+++ javaScript/prets.js
@@ -4,3 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch('http://127.0.0.1:8080/ords/bibliotheque/prêts/')
             .then(response => response.json())
             .then(data => {
+                pretsList.innerHTML = '';
+                data.items.forEach(prêt => {
+                    const col = document.createElement('div');
+                    col.className = 'col-md-4';
+                    col.innerHTML = `

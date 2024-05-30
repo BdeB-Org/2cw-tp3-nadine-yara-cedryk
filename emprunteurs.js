a0a65633d6f4dc311efba5516aaec58b30bf2df9
NADINE <2344893@bdeb.qc.ca>
2024-05-29 23:48:10 -0400

    Update emprunteurs.js

--- emprunteurs.js
+++ emprunteurs.js
@@ -9,3 +9,11 @@ document.addEventListener('DOMContentLoaded', function() {
                     const col = document.createElement('div');
                     col.className = 'col-md-4';
                     col.innerHTML = `
+                    <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${emprunteur.nom}</h5>
+                        <p class="card-text">Adresse: ${emprunteur.adresse}</p>
+                        <p class="card-text">Téléphone: ${emprunteur.telephone}</p>
+                    </div>
+                </div>
+            `;

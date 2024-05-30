d4fc80ec9c381391e698f654a426955e24d9f46a
NADINE <2344893@bdeb.qc.ca>
2024-05-29 23:38:51 -0400

    javaScript genres

---
+++ genres.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const genresList = document.getElementById('genres-list');
+    const searchButton = document.getElementById('search-button');
+    const searchInput = document.getElementById('search-nom');

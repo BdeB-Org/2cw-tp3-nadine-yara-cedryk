9af930d7af004079d4085ae551f8325a03e9d5e6
NADINE <2344893@bdeb.qc.ca>
2024-05-29 23:47:26 -0400

    Update emprunteurs.js

--- emprunteurs.js
+++ emprunteurs.js
@@ -1,2 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const emprunteursList = document.getElementById('emprunteurs-list');
+    function fetchEmprunteurs() {
+        fetch('http://127.0.0.1:8080/ords/bibliotheque/emprunteurs/')
+            .then(response => response.json())
+            .then(data => {
